6b9ff5827cad90ff4c6770e044f958d7924db94f
Alice Goulley <goulleyalice@gmail.com>
2021-02-15 15:02:10 +0100

    lib updated

--- projet_alice/lib.py
+++ projet_alice/lib.py
@@ -1,8 +1,14 @@
 def try_me():
-    x = input('do you have memes for me? yes/no')
+
+    while True:
+        x = input('do you have memes for me? yes/no ')
+        if x not in ['yes', 'Yes', 'No', 'no']:
+            print("Sorry, what are you saying you anoying cow")
+            continue
+        else:
+            break
+
     if x == 'yes' or x == 'Yes':
         return 'please send it to me'
     if x == 'no' or x == 'No':
         return 'what are you doing here then'
-    if x not in ['yes','Yes','no','No']:
-        'Please entre valid response you anoying cow'
